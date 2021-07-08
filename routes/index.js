7526d5e29c64189b983414f9501cd3a8d6f3f728
Oscar <oscar.miras@gmail.com>
2021-07-08 10:34:16 +0200

    practica preparada

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var {verificadorDocumento} = require('../utils/verificadores');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Verificador De Documentos' });
+  res.render('index', { title: 'Verificador De Documentos', esValido: 0 });
 });
+
+router.post('/verificar-documento', (req, res) => {
+
+  let esValido;
+  console.log(req.body.documento);
+  if (verificadorDocumento(req.body.documento)) {
+    esValido = 1;   
+  }
+
+  else {
+    esValido = -1;
+  }
+
+  res.render('index', { title: 'Verificador De Documentos', esValido });
+})
 
 module.exports = router;
---
+++ utils/verificadores.js
@@ -0,0 +1,6 @@
+exports.verificadorDocumento = (documento) => {
+    const patronDni = /[0-9]{8}[A-Z]/;
+    const esValido = patronDni.test(documento);
+
+    return esValido;
+}
