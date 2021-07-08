032d9b9e9a6d9c52f5083cec9e9afab2b773dafc
Oscar <oscar.miras@gmail.com>
2021-07-08 16:15:07 +0200

    bug, gracias Jose

--- utils/verificadores.js
+++ utils/verificadores.js
@@ -1,6 +1,6 @@
 exports.verificadorDocumento = (documento) => {
-    const patronDni = /[0-9]{8}-?[A-Z]/;
-    const patronNIE = /[A-Z][0-9]{7}[A-Z]/;
+    const patronDni = /^[0-9]{8}-?[A-Z]$/;
+    const patronNIE = /^[A-Z][0-9]{7}[A-Z]$/;
 
     const esValidoDNI = patronDni.test(documento);
     const esValidoNIE = patronNIE.test(documento);
